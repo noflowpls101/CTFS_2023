63c42f9227259f388fe14496e93c2fa4b20c9d7c
noflowpls <122080182+noflowpls101@users.noreply.github.com>
2023-05-04 00:24:45 +0630

    Delete JerseyCTF_2023/Solutions_Scripts directory

--- JerseyCTF_2023/Solutions_Scripts/i_got_the_keys.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {
-    'authorization_key': 'GdERHpBh3x'
-}
-
-r = requests.get("http://localhost:3000/flag", headers=headers)
-print(r.text)
--- JerseyCTF_2023/Solutions_Scripts/put_the_cookie_down.py
+++
@@ -1,6 +0,0 @@
-import requests
-import re
-
-r = requests.get("http://localhost:3001/")
-print(r.cookies)
-
